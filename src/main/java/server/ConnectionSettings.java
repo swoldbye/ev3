f37e370e69fc2f3828d0f00ee074014220e57463
harald <s185014@student.dtu.dk>
2020-03-11 13:23:12 +0100

    Test gitignore

--- src/main/java/server/ConnectionSettings.java
+++ src/main/java/server/ConnectionSettings.java
@@ -2,6 +2,7 @@ package server;
 
 public class ConnectionSettings {
 
+    // TODO: IKKE PUSH DIN EGEN PORT OG IP!!
     public static final int PORT = 4444;
-    public static final String IP = "172.20.10.8";
+    public static final String IP = "172.20.10.100";
 }
